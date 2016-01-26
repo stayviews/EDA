55e81b244167c135ce6a61f5d18b4d426f33e50a
stayviews <309160734@qq.com>
2016-01-26 15:30:05 +0800

    ok

--- EDA/Weapon/Weapon.h
+++ EDA/Weapon/Weapon.h
@@ -189,7 +189,7 @@ public:
 	/** Called in network play to stop cosmetic fx (e.g. for a looping shot). */
 	virtual void StopSimulatingWeaponFire();
 	/** play weapon animations */
-	float PlayWeaponAnimation(UAnimMontage* Animation);
+	float PlayWeaponAnimation(UAnimMontage* Animation,float InPlayRate=1.0f);
 
 	/** stop playing weapon animations */
 	void StopWeaponAnimation(UAnimMontage* Animation);
